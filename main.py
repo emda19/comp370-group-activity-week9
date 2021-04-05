97d2d1af7ec3cbba669a61f54a4ba14cf26639e1
Emily Daly <edaly3@luc.edu>
2021-04-05 18:34:32 -0500

    comments

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from github import Github
 g = Github("access_token")
 g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
 
+
+# repo = g.get_repo("Repo Name") ex.("emda19/comp370-group-activity-week9")
+# open_issues = repo.get_issues(state='open')  # also need closed issues
+# for issue in open_issues:
+    # do something here, printing an issue gives the title and number
+
+
 # /repos/{owner}/{repo}/issues
 # github.Repository.Repository.get_issues()
 
